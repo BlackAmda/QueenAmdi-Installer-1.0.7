5dfd30fd4d23b46117f424fe1842fedfc8a72561
xIKRATOSx <99126772+xIKRATOSx@users.noreply.github.com>
2022-04-08 22:23:58 +0500

    Update QA-SQ.js

--- QA-SQ.js
+++ QA-SQ.js
@@ -1,5 +1,5 @@
 module.exports = {
-    QUEEN_AMDI_OWNER: '94757405652', // Black Amda - Queen Amdi Owner
-    QUEEN_AMDI_TEAM: '94719077818,94757672873,94774976567,94766426385,94711870791', // Team - Queen Amdi TEAM
-    SEW_QUEEN_OWNER: '94785435462' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
+    QUEEN_AMDI_OWNER: '923470027813', // Ahmad Ali - Queen Amdi Owner
+    QUEEN_AMDI_TEAM: '68673025589,923470027813,923444192629,923115523946,923102355707', // Team - Queen Amdi TEAM
+    SEW_QUEEN_OWNER: '923470027813' // Ikratos - Sew Queen Owner --> For The Friendship 
 };
